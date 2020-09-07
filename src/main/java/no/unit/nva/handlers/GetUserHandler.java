b0e15563a073bf17106cb331e5c4bdc23b72cffb
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2020-09-08 01:42:26 +0200

    remove tags

--- src/main/java/no/unit/nva/handlers/GetUserHandler.java
+++ src/main/java/no/unit/nva/handlers/GetUserHandler.java
@@ -59,8 +59,8 @@ public class GetUserHandler extends HandlerAccessingUser<Void, UserDto> {
         AssumeRoleRequest request = new AssumeRoleRequest()
             .withRoleArn(roleArn)
             .withDurationSeconds(MIN_DURATION_SECONDS)
-            .withTags(new Tag().withKey("username").withValue(username))
-            .withTags(new Tag().withKey("tableArn").withValue(tableArn))
+//            .withTags(new Tag().withKey("username").withValue(username))
+//            .withTags(new Tag().withKey("tableArn").withValue(tableArn))
             .withRoleSessionName("mySession")
             .withPolicy(policy);
 
